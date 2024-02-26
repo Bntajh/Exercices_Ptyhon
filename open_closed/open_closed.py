ff457586ca31f08dc4c88df578b2ca1589fa20d5
becker <becker@debian>
2024-02-26 11:11:07 +0100

    feat:condition open_closed

--- open_closed/open_closed.py
+++ open_closed/open_closed.py
@@ -1,39 +1,24 @@
 def open_closed(s: str) -> bool:
     temps = []
-    ouvrant = "({<['\""
+    ouvrant = "({<[\"'"
     fermant = ")}>]\"'"
     chaine_correspondance = {')': '(', ']': '[', '}': '{', '>': '<', "'": "'", '"': '"'}
     
     for char in s:
-        if char == '"':
-            if not temps or temps[-1] != '"':
-                temps.append('"')
-            else:
-                temps.pop()
-        elif char in ouvrant:   
+        if char in ouvrant:
             temps.append(char)
         elif char in fermant:
             if not temps or temps[-1] != chaine_correspondance[char]:
                 return False
             temps.pop() 
 
-    return len(temps) == 0
+    return not temps
 
-'''tests = [
-    "()",     
-    "][",    
-    "([])",  
-    "(",     
-    "(()",    
-    "[(])",   
-    "[)",    
-    '""',    
-    " '\"",    
+
+tests = [
+    "[(])",      
 ]
 
-test_number = 1
 for test in tests:
     result = open_closed(test)
-    print(f"Test {test_number}: {test}\tRésultat: {result}")
-    test_number += 1
-'''
+    print(f"result:{test}: {result}")
