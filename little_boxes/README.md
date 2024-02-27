56a803c967a8d78a7c3183cfae2d7cc57c54a4fa
becker <becker@debian>
2024-02-27 16:21:03 +0100

    feat:little_boxes ok

---
+++ little_boxes/README.md
@@ -0,0 +1,7 @@
+Solution Propose :
+
+J'ai créé une fonction nommée little_boxes qui prend en paramètre une chaîne de caractères s.
+
+Ensuite, j'ai utilisé la fonction sorted() qui trie tous les éléments de la chaîne s dans l'ordre ASCII.
+
+Enfin, j'ai utilisé la méthode .join() pour concaténer les éléments de la chaîne de caractères triée, formant ainsi une nouvelle chaîne de caractères.
---
+++ open_closed/open_closed.py
@@ -0,0 +1,24 @@
+def open_closed(s: str) -> bool:
+    temps = []
+    ouvrant = "({<[\"'"
+    fermant = ")}>]\"'"
+    chaine_correspondance = {')': '(', ']': '[', '}': '{', '>': '<', "'": "'", '"': '"'}
+    
+    for char in s:
+        if char in ouvrant:
+            temps.append(char)
+        elif char in fermant:
+            if not temps or temps[-1] != chaine_correspondance[char]:
+                return False
+            temps.pop() 
+     
+    return not temps
+
+
+tests = [
+    "[(])",      
+]
+
+for test in tests:
+    result = open_closed(test)
+    print(f"result:{test}: {result}")
