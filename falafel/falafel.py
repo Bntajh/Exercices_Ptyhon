7aad91c03be3a4972ed9c85fcdde90accc093dcb
ibrahim <ibrahim@debian>
2024-02-29 10:53:12 -0500

    feat : couppage de string en deux

--- falafel/falafel.py
+++ falafel/falafel.py
@@ -1,13 +1,10 @@
 def falafel(s: str) -> bool :
     tab_str1 = []
     tab_str2 = []
-    for i in range (len(str)):
-        
-
-
-
-
-
-
-
+    
+    for i in range (len(str)/2):
+        tab_str1.append(str[i])
+    for j in range (len(str)/2):
+        tab_str2.append(str[i])
+    
     return 0
