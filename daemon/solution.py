d1468ce28bee5c6a7994789ab345364c50639942
Fatoumata Binta Diallo <bintadiallo@MacBook-Air-de-Fatoumata.local>
2024-03-19 15:14:43 +0100

    feat: création du readme

--- daemon/solution.py
+++ daemon/solution.py
@@ -1,21 +1,6 @@
 from typing import List
 
 def daemon(numbers: List[int], k: int)  -> bool:
-    left = 0
-    right = len(numbers) - 1
-
-    if k < 0 or k >= len(numbers):
-        return False
-    
-    while left < k:
-        if numbers[left] > numbers[k]:
-            return False
-        left += 1
-        
-    while k < right:
-        if numbers[right] < numbers[k]:
-            return False
-        right -= 1
 
     left = 0
     right = len(numbers) - 1
