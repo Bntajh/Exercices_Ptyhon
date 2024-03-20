10511a17285bc089fd803aa5dfd1fb71eec3641b
Fatoumata Binta Diallo <bintadiallo@MacBook-Air-de-Fatoumata.local>
2024-03-20 15:17:21 +0100

    perf: correction des erreurs

--- daemon/solution.py
+++ daemon/solution.py
@@ -3,6 +3,9 @@ from typing import List
 def daemon(numbers: List[int], k: int) -> bool:
     if k < 0 or k >= len(numbers):
         return False
+    
+    if all(num == numbers[k] for num in numbers):
+        return True
 
     for i in range(len(numbers)):
         if i != k:
@@ -12,9 +15,10 @@ def daemon(numbers: List[int], k: int) -> bool:
                 return False
     return True
 
-# numbers = [100, 97, 101, 109, 111, 110]
-# k = 3
-# print(daemon(numbers, k))
+
+numbers = [100, 97, 101, 109, 111, 110]
+k = 1
+print(daemon(numbers, k))
 
 
 
