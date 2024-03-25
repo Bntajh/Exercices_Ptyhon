318df66b66f201c8c6e91b7e4b2480531c105e61
user1 <user1@debian.domain.com>
2024-03-25 22:04:43 +0100

    version 1 de ma partie

---
+++ yin_yang/solution.py
@@ -0,0 +1,18 @@
+def yin_yang(s: str) -> bool:
+    stack = []
+    opening_chars = {'(', '[', '{', '"', "'"}
+    closing_chars = {')', ']', '}', '"', "'"}
+    matching_pairs = {')': '(', ']': '[', '}': '{', '"': '"', "'": "'"}
+    
+    for char in s:
+        if char in opening_chars:
+            stack.append(char)
+        elif char in closing_chars:
+            if not stack or stack.pop() != matching_pairs[char]:
+                return False
+
+    return not stack 
+    
+input_str = "([])"
+result = yin_yang(input_str)
+print(result)
