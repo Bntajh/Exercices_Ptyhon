e09fb0d918adaced296bfbf658050fb99df8c1ce
ibrahim <ibrahim@debian>
2024-03-29 06:12:10 -0400

    fix : retourner -1 si tableau vide

--- tux/solution.py
+++ tux/solution.py
@@ -1,4 +1,7 @@
 def tux(numbers: list[int]) -> int:
+    if not numbers:
+        return -1
+
     n = len(numbers)
     min_num = min(numbers)
     max_num = max(numbers)
@@ -6,14 +9,11 @@ def tux(numbers: list[int]) -> int:
     count_num = [0] * range_num
     pos_num = [-1] * n
 
-
     for i in range(n):
         count_num[numbers[i] - min_num] += 1
 
-
     for i in range(1, range_num):
         count_num[i] += count_num[i - 1]
-
 
     for i in range(n - 1, -1, -1):
         count_num[numbers[i] - min_num] -= 1
