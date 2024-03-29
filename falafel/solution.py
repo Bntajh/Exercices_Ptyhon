ef7dca9ab303c8ebf993e6fb9c3268686ee70dad
ibrahim <ibrahim@debian>
2024-03-29 06:02:25 -0400

    refactor : retrait de l'appel à la fonction → tux feat: rajout de permutation fonction → falafel

--- falafel/solution.py
+++ falafel/solution.py
@@ -1,13 +1,22 @@
-def falafel(s: str) -> bool:
+def falafel (s: str) -> bool:
+    if len(s) <= 1:
+        return True
+
+    char_count = {}
     middle = len(s) // 2
-    res = True
 
     for i in range(middle):
-        if s[i] != s[len(s) - 1 - i]:
-            res =  False
-    
-    return res
+        if s[i] in char_count:
+            char_count[s[i]] += 1
+        else:
+            char_count[s[i]] = 1
 
-# Test de la fonction
-print(falafel("lalal"))  # True
-print(falafel("test"))  # False
+    for j in range(middle, len(s)):
+        if s[j] in char_count:
+            char_count[s[j]] -= 1
+            if char_count[s[j]] == 0:
+                del char_count[s[j]]
+        if len(char_count) > 1:
+            return False
+
+    return True
