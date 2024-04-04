732905604c9639d9d572a673d08f577634433730
becker <becker@debian>
2024-04-04 21:54:55 +0200

    feat:daemona jour

--- daemon/solution.py
+++ daemon/solution.py
@@ -1,18 +1,20 @@
 from typing import List
 
-def daemon(numbers: List[int], k: int)  -> bool:
-    for i in range(len(numbers)):
-        if i != k:
-            if i < k and numbers[i] >= numbers[k]:
-                return False
-            elif i > k and numbers[i] <= numbers[k]:
-                return False
-    return True
+def daemon(numbers: List[int], k: int) -> bool:
+    if not numbers or k < 0 or k >= len(numbers):
+        return False  
+    pivot = numbers[k]
+    max_avant_k = max(numbers[:k], default=pivot)
+    min_apres_k = min(numbers[k+1:], default=pivot)
+    
+    return max_avant_k < pivot and min_apres_k >= pivot
 
 
-# numbers = [100, 97, 101, 109, 111, 110]
-# k = 1
-# print(daemon(numbers, k))
+#test = [
+ #   ([100, 97, 101, 109, 111, 110], 1), 
+  #  ([100, 97, 101, 109, 111, 110], 2),  
+  #  ([100, 97, 101, 109, 111, 110], 3)  
+#]
 
-
-
+#for numbers, k in test:
+#    print(daemon(numbers, k))
