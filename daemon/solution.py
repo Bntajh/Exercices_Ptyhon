0279e76f261d68d8822981a0cd2e1ae17d2c4150
Fatoumata Binta Diallo <bintadiallo@AirdeFatoumata.lan>
2024-04-04 22:23:14 +0200

    perf: changements dans le code

--- daemon/solution.py
+++ daemon/solution.py
@@ -1,17 +1,17 @@
 from typing import List
 
-def daemon(numbers: List[int], k: int)  -> bool:
-    for i in range(len(numbers)):
-        if i != k:
-            if i < k and numbers[i] >= numbers[k]:
-                return False
-            elif i > k and numbers[i] <= numbers[k]:
-                return False
-    return True
+def daemon(numbers: List[int], k: int) -> bool:
+    pivot = numbers[k]
+    left = all(numbers[i] <= pivot 
+                        for i in range(k) if i != k)
+    right = all(numbers[i] >= pivot 
+                        for i in range(k + 1, len(numbers)) if i != k)
+    return left and right
+
 
 
 # numbers = [100, 97, 101, 109, 111, 110]
-# k = 1
+# k = 3
 # print(daemon(numbers, k))
 
 
