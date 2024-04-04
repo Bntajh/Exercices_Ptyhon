d890142a32a49e619b007858cf785539c3255586
Fatoumata Binta Diallo <bintadiallo@MacBook-Air-de-Fatoumata.local>
2024-04-04 18:11:06 +0200

    perf: changements dans le code

--- daemon/solution.py
+++ daemon/solution.py
@@ -4,9 +4,9 @@ def daemon(numbers: List[int], k: int)  -> bool:
 
     for i in range(len(numbers)):
         if i != k:
-            if i < k and numbers[i] > numbers[k]:
+            if i < k and numbers[i] >= numbers[k]:
                 return False
-            elif i > k and numbers[i] < numbers[k]:
+            elif i > k and numbers[i] <= numbers[k]:
                 return False
     return True
 
