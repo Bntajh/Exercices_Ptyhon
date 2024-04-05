9c2f283fab9959c0a85fb9b4da041b4011b277aa
becker <becker@debian>
2024-04-05 11:00:49 +0200

    refactor:daemon

--- daemon/solution.py
+++ daemon/solution.py
@@ -1,18 +1,15 @@
-from typing import List
+def daemon(numbers, k):
+    if k < 0 or k >= len(numbers):
+        return False
 
-def daemon(numbers: List[int], k: int)  -> bool:
     for i in range(len(numbers)):
-        if i != k:
-            if i < k and numbers[i] >= numbers[k]:
-                return False
-            elif i > k and numbers[i] <= numbers[k]:
-                return False
+        if i < k and numbers[i] >= numbers[k]:
+            return False
+
+        if i > k and numbers[i] < numbers[k]:
+            return False
+
     return True
-
-
 # numbers = [100, 97, 101, 109, 111, 110]
-# k = 1
-# print(daemon(numbers, k))
-
-
-
+# k = 2
+# print(daemon(numbers, k))  
