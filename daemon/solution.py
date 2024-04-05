098ee528170be195505ce02fc6abb5dd60070af6
becker <becker@debian>
2024-04-05 11:15:43 +0200

    refactor:daemon

--- daemon/solution.py
+++ daemon/solution.py
@@ -1,15 +1,15 @@
-from typing import List
+def daemon(numbers, k):
+    if k < 0 or k >= len(numbers):
+        return False
 
-def daemon(numbers: List[int], k: int) -> bool:
-    pivot = numbers[k]
-    left = all(num <= pivot for num in numbers[:k])
-    right = all(num >= pivot for num in numbers[k + 1:])
-    return left and right
+    for i in range(len(numbers)):
+        if i < k and numbers[i] >= numbers[k]:
+            return False
 
+        if i > k and numbers[i] < numbers[k]:
+            return False
 
-
-
+    return True
 # numbers = [100, 97, 101, 109, 111, 110]
-# k = 3
-# print(daemon(numbers, k))
-
+# k = 2
+# print(daemon(numbers, k))  
